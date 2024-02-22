7db6ffd0541abf6b2fc3245f3233adfadfd9835a
younghaadam <adamkehinde@gmail.com>
2024-02-23 00:04:47 +0100

    i comment one of the icons out on the footers component

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -17,10 +17,10 @@ const Footer = () => {
                     <div className="col-md-6 col-lg-6">
                         <ul className='p-0'>
                             <h3 className="heading-section">Contact Info</h3>
-                            <li><span className="icon icon-map-marker">203 Fake St. Mountain View, San <br />Francisco, California, USA</span></li>
+                            <li><span className="icon icon-map-marker"><FaMapMarker style={{ color: '#79ccab', fontSize: '1rem', marginRight: "10px" }} /> 203 Fake St. Mountain View, San <br />Francisco, California, USA</span></li>
                             <li><a href="#"><span className="icon icon-phone text-white"><FaPhone style={{ color: '#79ccab', fontSize: '1rem', marginRight: '10px' }} /> +2 392 3929 210</span></a></li>
                             <li><a href="#"><span className="icon icon-envelope text-white"><FaEnvelope style={{ color: '#79ccab', fontSize: '1rem', marginRight: "10px" }} />   info@yourdomain.com</span></a></li>
-                            <li><a href="#"><span className="icon icon-envelope text-white"><FaMapMarker style={{ color: '#79ccab', fontSize: '1rem', marginRight: "10px" }} />   San Francisco, IL</span></a></li>
+                            {/* <li><a href="#"><span className="icon icon-envelope text-white"><FaMapMarker style={{ color: '#79ccab', fontSize: '1rem', marginRight: "10px" }} />   San Francisco, IL</span></a></li> */}
                             <li><span className="icon icon-clock-o"><FaClock style={{ color: '#79ccab', fontSize: '1rem', marginRight: "10px" }} /> Monday — Friday 8:00am - 5:00pm</span></li>
                         </ul>
                     </div>
